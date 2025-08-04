5b1d84776aea19c4f27c4a03641f7efa1921bb58
Conrad Lippert-Zajaczkowski <ceo@swapped.shop>
2025-08-04 12:26:18 -0500

    1m

--- user_data/strategies/WarriorMomentum.py
+++ user_data/strategies/WarriorMomentum.py
@@ -93,7 +93,7 @@ class WarriorMomentum(IStrategy):
     # trailing_only_offset_is_reached = True
 
     # Optimal timeframe for the strategy.
-    timeframe = "5m"
+    timeframe = "1m"
 
     # Run "populate_indicators()" only for new candle.
     process_only_new_candles = True
